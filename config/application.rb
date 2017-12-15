7d8efef60aa1b7df500628a6ace40cee160c5eb7
upinetree <upinetree@gmail.com>
2017-12-15 09:50:34 +0900

    Configure application.rb

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
@@ -25,7 +25,10 @@ module SampleComplexViews
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.system_tests = nil
+      g.assets = false
+      g.helper = false
+    end
   end
 end
